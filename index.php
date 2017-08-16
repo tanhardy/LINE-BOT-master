457cd94367172dbaff7f3dc010de222fd5a8096c
jaratsang <31063341+jaratsang@users.noreply.github.com>
2017-08-16 16:35:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+  $arrPostData['messages'][0]['text'] = "กินข้าว";
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
